fcb5c1575480223aed08440077e576a360556064
Egor Kin <egorkin@gmail.com>
2015-11-03 21:46:10 +0300

    Update reg.cpp

--- reg.cpp
+++ reg.cpp
@@ -1,4 +1,4 @@
-#include "stm8.hpp"
+#include "st8.hpp"
 #include <fpro.h>
 #include <diskio.hpp>
 #include <entry.hpp>
@@ -8,7 +8,7 @@ static const char *register_names[] =
 {
   "a", "x", "y", "cc", "sp",
   "xl", "xh", "yl", "yh",
-  "ds", "cs"
+  "ds", "cs",
 };
 
 //--------------------------------------------------------------------------
@@ -25,9 +25,9 @@ static bytes_t retcodes[] =
 //      STMicroelectronics - Assembler - rel. 4.10
 //      We support Motorola format
 //-----------------------------------------------------------------------
-static const char *stm8_header[] =
+static const char *st8_header[] =
 {
-  "stm8/",
+  "st8/",
   "",
   NULL
 };
@@ -41,9 +41,9 @@ static asm_t stasm =
   AS_NOXRF|     // Disable xrefs during the output file generation
   AS_ONEDUP,    // one array definition per line
   0,
-  "STMicroelectronics - Assembler8",
+  "STMicroelectronics - Assembler",
   0,
-  stm8_header,   // header lines
+  st8_header,   // header lines
   NULL,         // no bad instructions
   "org",        // org
   "end",        // end
@@ -146,84 +146,73 @@ const char * idaapi set_idp_options(const char *keyword,int /*value_type*/,const
 //--------------------------------------------------------------------------
 netnode helper;
 
-static int notify(processor_t::idp_notify msgid, ...)
+static int idaapi notify(processor_t::idp_notify msgid, ...)
 {
-	int retcode = 1;
-	va_list va;
-	va_start(va, msgid);
+  va_list va;
+  va_start(va, msgid);
 
-	// A well behaving processor module should call invoke_callbacks()
-	// in his notify() function. If this function returns 0, then
-	// the processor module should process the notification itself
-	// Otherwise the code should be returned to the caller:
+// A well behaving processor module should call invoke_callbacks()
+// in his notify() function. If this function returns 0, then
+// the processor module should process the notification itself
+// Otherwise the code should be returned to the caller:
 
-	int code = invoke_callbacks(HT_IDP, msgid, va);
-	if ( code ) return code;
+  int code = invoke_callbacks(HT_IDP, msgid, va);
+  if ( code ) return code;
 
-	switch(msgid)
-	{
-	case processor_t::init:
-		helper.create("$ stm8");
-		inf.mf = 1;
-		break;
+  switch ( msgid )
+  {
+    case processor_t::init:
+      helper.create("$ st8");
+      inf.mf = 1;
+    default:
+      break;
 
-	case processor_t::newfile:  // new file loaded
-		{
-			char cfgfile[QMAXFILE];
-			get_cfg_filename(cfgfile, sizeof(cfgfile));
-			if ( choose_ioport_device(cfgfile, device, sizeof(device), NULL) )
-				set_device_name(device, IORESP_ALL);
-			create_words();
-		}
-		break;
+    case processor_t::newfile:  // new file loaded
+      {
+        char cfgfile[QMAXFILE];
+        get_cfg_filename(cfgfile, sizeof(cfgfile));
+        if ( choose_ioport_device(cfgfile, device, sizeof(device), NULL) )
+          set_device_name(device, IORESP_ALL);
+        create_words();
+      }
+      break;
 
-	case processor_t::oldfile:  // old file loaded
-		{
-			char buf[MAXSTR];
-			if ( helper.supval(-1, buf, sizeof(buf)) > 0 )
-				set_device_name(buf, IORESP_NONE);
-		}
-		break;
+    case processor_t::oldfile:  // old file loaded
+      {
+        char buf[MAXSTR];
+        if ( helper.supval(-1, buf, sizeof(buf)) > 0 )
+          set_device_name(buf, IORESP_NONE);
+      }
+      break;
 
-	case processor_t::is_jump_func:
-		{
-			const func_t *pfn = va_arg(va, const func_t *);
-			ea_t *jump_target = va_arg(va, ea_t *);
-			retcode = is_jump_func(pfn, jump_target);
-		}
-		break;
+    case processor_t::is_jump_func:
+      {
+        const func_t *pfn = va_arg(va, const func_t *);
+        ea_t *jump_target = va_arg(va, ea_t *);
+        return is_jump_func(pfn, jump_target);
+      }
 
-	case processor_t::is_sane_insn:
-		retcode = is_sane_insn(va_arg(va, int));
-		break;
+    case processor_t::is_sane_insn:
+      return is_sane_insn(va_arg(va, int));
 
-	case processor_t::may_be_func:
-		// can a function start here?
-		// arg: none, the instruction is in 'cmd'
-		// returns: probability 0..100
-		// 'cmd' structure is filled upon the entrace
-		// the idp module is allowed to modify 'cmd'
-		retcode = may_be_func();
-		break;
+    case processor_t::may_be_func:
+                                // can a function start here?
+                                // arg: none, the instruction is in 'cmd'
+                                // returns: probability 0..100
+                                // 'cmd' structure is filled upon the entrace
+                                // the idp module is allowed to modify 'cmd'
+      return may_be_func();
 
-	case processor_t::get_autocmt:
-		{
-			char *buf = va_arg(va, char *);
-			size_t bufsize = va_arg(va, size_t);
-			if ( qsnprintf(buf, bufsize, "%s", insn_auto_cmts[cmd.itype]) )
-				retcode = 2;
-		}
-		break;
-	}
-
-	va_end(va);
-	return retcode;
+  }
+  va_end(va);
+  return 1;
 }
 
 //-----------------------------------------------------------------------
-static char const *shnames[] = { "stm8", NULL };
-static char const *lnames[] = {
-  "SGS-Thomson STM8",
+#define FAMILY "SGS-Thomson ST8:"
+static const char *const shnames[] = { "st8", NULL };
+static const char *const lnames[] = {
+  FAMILY"SGS-Thomson ST8",
   NULL
 };
 
@@ -233,13 +222,13 @@ static char const *lnames[] = {
 processor_t LPH =
 {
   IDP_INTERFACE_VERSION,        // version
-  0x8000 | 1234,                // id
+  0x8000,                     // id
   PRN_HEX|PR_RNAMESOK,
   8,                            // 8 bits in a byte for code segments
   8,                            // 8 bits in a byte for other segments
 
-  (char const **)shnames,
-  (char const **)lnames,
+  shnames,
+  lnames,
 
   asms,
 
@@ -263,7 +252,7 @@ processor_t LPH =
   NULL,                 // can have type
 
   qnumber(register_names), // Number of registers
-  (char const **)register_names,       // Register names
+  register_names,       // Register names
   NULL,                 // get abstract register
 
   0,                    // Number of register files
@@ -279,8 +268,8 @@ processor_t LPH =
   NULL,                 // No known code start sequences
   retcodes,
 
-  STM8_null,
-  STM8_last,
+  ST8_null,
+  ST8_last,
   Instructions,
 
   NULL,                 // int  (*is_far_jump)(int icode);
@@ -293,16 +282,16 @@ processor_t LPH =
                         // normal float
                         // normal double
                         // long double
-  is_switch,            // bool idaapi is_switch(switch_info_ex_t *si);
-  NULL,                 // long (*gen_map_file)(FILE *fp);
-  NULL,                 // ulong (*extract_address)(ulong ea,const char *string,int x);
+  NULL,                 // int (*is_switch)(switch_info_t *si);
+  NULL,                 // int32 (*gen_map_file)(FILE *fp);
+  NULL,                 // ea_t (*extract_address)(ea_t ea,const char *string,int x);
   NULL,                 // int (*is_sp_based)(op_t &x);
   NULL,                 // int (*create_func_frame)(func_t *pfn);
   NULL,                 // int (*get_frame_retsize(func_t *pfn)
-  NULL,                 // void (*gen_stkvar_def)(char *buf,const member_t *mptr,long v);
+  NULL,                 // void (*gen_stkvar_def)(char *buf,const member_t *mptr,int32 v);
   gen_spcdef,           // Generate text representation of an item in a special segment
-  STM8_ret,             // Icode of return instruction. It is ok to give any of possible return instructions
+  ST8_ret,              // Icode of return instruction. It is ok to give any of possible return instructions
   set_idp_options,      // const char *(*set_idp_options)(const char *keyword,int value_type,const void *value);
-  is_align_insn,        // int (*is_align_insn)(ulong ea);
+  is_align_insn,        // int (*is_align_insn)(ea_t ea);
   NULL,                 // mvm_t *mvm;
 };
